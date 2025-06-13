fdc4f5a25a98f26ae9c1a3a8b00c350957044480
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2025-06-13 17:24:57 +0200

    Feature: Improved code readability and output generation in console.

--- src/com/umer/main/Executor.java
+++ src/com/umer/main/Executor.java
@@ -1,10 +1,13 @@
 package com.umer.main;
 
+import java.util.Arrays;
+
 import com.umer.service.TwoSumIndicesFinderService;
 
 public class Executor {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Finding indices of two numbers such that they add up to a specific target number.");
+        giveGapInConsole();
         /**
          * Question: Given an array of integers, find two numbers such that they add up
          * to a specific target number. Implement a function that returns the indices of
@@ -18,33 +21,53 @@ public class Executor {
         int[] nums = { 2, 7, 11, 15 };
         int target = 9;
         int[] result = twoSumIndicesFinder.findIndices(nums, target);
-        displayResult(result);
+        displayContentsOfInputArray(nums);
+        displayResult(result, target);
+        giveGapInConsole();
 
         target = 22;
         result = twoSumIndicesFinder.findIndices(nums, target);
-        displayResult(result);
+        displayContentsOfInputArray(nums);
+        displayResult(result, target);
+        giveGapInConsole();
 
         target = 21;
         result = twoSumIndicesFinder.findIndices(nums, target);
-        displayResult(result);
+        displayContentsOfInputArray(nums);
+        displayResult(result, target);
+        giveGapInConsole();
 
         int[] negativeNums = { -2, 7, 11, 15 };
         target = 5;
         result = twoSumIndicesFinder.findIndices(negativeNums, target);
-        displayResult(result);
+        displayContentsOfInputArray(negativeNums);
+        displayResult(result, target);
+        giveGapInConsole();
 
         int[] duplicateNums = { 2, 7, 7, 11, 15 };
         target = 14;
         result = twoSumIndicesFinder.findIndices(duplicateNums, target);
-        displayResult(result);
+        displayContentsOfInputArray(duplicateNums);
+        displayResult(result, target);
+        giveGapInConsole();
 
     }
 
-    private static void displayResult(int[] result) {
+    private static void giveGapInConsole() {
+        System.out.println();
+        System.out.println();
+    }
+
+    private static void displayContentsOfInputArray(int[] duplicateNums) {
+        System.out.println("The input array: " + Arrays.toString(duplicateNums));
+    }
+
+    private static void displayResult(int[] result, int target) {
         if (result.length < 1) {
-            System.out.println("No two corresponding indices found.");
+            System.out.println("For the target " + target + " ,no two corresponding indices found.");
         } else {
-            System.out.println("Indices: " + result[0] + ", " + result[1]);
+            System.out.println("The target " + target + " is a sum of the following two Indices: " + result[0] + ", "
+                    + result[1] + " in the input array.");
         }
     }
 }
